04dba7df88cd2161cc8d00827850f9874ea88e96
Adam Ewing <adam.ewing@gmail.com>
2014-03-25 14:04:42 +1000

    bwa-mem alignment working, some testing cruft

--- rcseq.py
+++ rcseq.py
@@ -1,9 +1,121 @@
 #!/usr/bin/env python
 
+import argparse
 import subprocess
+import gzip
+import os
+import sys
+
+from uuid import uuid4
+from itertools import izip
+
+'''
+rcseq.py: Analyse RC-seq datasets.
+
+Prereqs:
+exonerate
+bwa
+pysam
+
+'''
+
 
 def rc(dna):
     complements = string.maketrans('acgtrymkbdhvACGTRYMKBDHV', 'tgcayrkmvhdbTGCAYRKMVHDB')
     return dna.translate(complements)[::-1]
+
+
+def align(qryseq, refseq):
+    ''' find best alignment with exonerate '''
+    rnd = str(uuid4()) 
+    tgtfa = 'tmp.' + rnd + '.tgt.fa'
+    qryfa = 'tmp.' + rnd + '.qry.fa'
+
+    tgt = open(tgtfa, 'w')
+    qry = open(qryfa, 'w')
+
+    tgt.write('>ref' + '\n' + refseq + '\n')
+    qry.write('>qry' + '\n' + qryseq + '\n')
+
+    tgt.close()
+    qry.close()
+
+    cmd = ['exonerate', '--bestn', '1', '-m', 'ungapped', '--showalignment','0', '--ryo', 'SUMMARY\t%s\t%qab\t%qae\t%tab\t%tae\n', qryfa, tgtfa]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    best = []
+    topscore = 0
+
+    for pline in p.stdout.readlines():
+        if pline.startswith('SUMMARY'):
+            c = pline.strip().split()
+            if int(c[1]) > topscore:
+                topscore = int(c[1])
+                best = c
+
+    os.remove(tgtfa)
+    os.remove(qryfa)
+
+    return best
+
+
+def bwamem(fq, ref, threads=1, width=150, sortmem=2000000000):
+    ''' FIXME '''
+    rg = '@RG\tID:RCSEQ\tSM:' + fq
+
+    sam_out  = fq + '.realign.sam'
+    bam_out  = fq + '.realign.bam'
+    sort_out = fq + '.realign.sorted'
+
+    sam_cmd  = ['bwa', 'mem', '-t', str(threads), '-M', '-R', rg, ref, fq]
+    bam_cmd  = ['samtools', 'view', '-bt', ref + '.bai', '-o', bam_out, sam_out]
+    sort_cmd = ['samtools', 'sort', '-@', str(threads), '-m', str(sortmem), bam_out, sort_out]
+    idx_cmd  = ['samtools', 'index', sort_out + '.bam']
+
+    sys.stderr.write("running bwa-mem: " + ' '.join(sam_cmd) + "\n")
+
+    with open(sam_out, 'w') as sam:
+        p = subprocess.Popen(sam_cmd, stdout=subprocess.PIPE)
+        for line in p.stdout:
+            sam.write(line)
+
+    sys.stderr.write("writing " + sam_out + " to BAM...\n")
+    subprocess.call(bam_cmd)
+
+    sys.stderr.write("sorting output: " + ' '.join(sort_cmd) + "\n")
+    subprocess.call(sort_cmd)
+
+    sys.stderr.write("indexing: " + ' '.join(idx_cmd) + "\n")
+    subprocess.call(idx_cmd)
+
+    return sort_out + '.bam'
+
+
+def flash_wrapper(fq1, fq2, max_overlap, threads):
+    ''' wrapper for FLASH (http://ccb.jhu.edu/software/FLASH/) '''
+    out = 'RCTMP-' + str(uuid4())
+    os.mkdir(out)
+    args = ['flash', '-d', out, '-o', out, '-M', str(max_overlap), '-z', '-t', str(threads), fq1, fq2]
+    print "calling FLASH:", args
+
+    subprocess.call(args)
+
+    return out + '/' + out + '.extendedFrags.fastq.gz'
+
+def main(args):
+    mergefq = flash_wrapper(args.pair1, args.pair2, args.maxoverlap, args.threads)
+    outbam  = bwamem(mergefq, args.ref, threads=args.threads)
+    
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Analyse RC-seq data')
+    parser.add_argument('-1', dest='pair1', required=True, help='fastq(.gz) containing first end reads')
+    parser.add_argument('-2', dest='pair2', required=True, help='fastq(.gz) containint second end reads')
+    parser.add_argument('-t', dest='threads', default=1, help='number of threads')
+    parser.add_argument('-r', dest='ref', required=True, help='reference genome for bwa-mem, also expects .fai index (samtools faidx ref.fa)')
+    parser.add_argument('--max-overlap', dest='maxoverlap', default=100, help='Maximum overlap used for joining paired reads with FLASH')
+    args = parser.parse_args()
+    main(args)
 
 

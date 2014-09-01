430220baa52abb6b1bbe452a67c7c4f98138733f
Adam Ewing <adam.ewing@gmail.com>
2014-09-01 10:10:27 +1000

    update coverage script

--- scripts/RCSEQCover.py
+++ scripts/RCSEQCover.py
@@ -104,53 +104,53 @@ def overlap(bamfn, bedfn, outdir, tabix=False):
 
     with open(bedfn, 'r') as bed:
         for line in bed:
-            lib = 'sampled'
-
             chrom, start, end, strand, name, eltstart, eltend = line.strip().split()
             elt5p, elt3p, eltstart, eltend = map(int, (start, end, eltstart, eltend))
 
             if strand == '-':
                 elt5p, elt3p = elt3p, elt5p
 
+            # initialize counters
+            if tabix:
+                libs = ['sampled']
+            else:
+                assert 'RG' in bam.header
+                libs = [rg['ID'] for rg in bam.header['RG']]
+
+            for lib in libs:
+                if lib not in overlaps:
+                    overlaps[lib] = {}
+
+                overlaps[lib][line.strip()] = {}
+                overlaps[lib][line.strip()]['5pNonDup'] = 0
+                overlaps[lib][line.strip()]['3pNonDup'] = 0
+                overlaps[lib][line.strip()]['5pAll'] = 0
+                overlaps[lib][line.strip()]['3pAll'] = 0
+
+
             # count 5p junction overlaps
             for read in bam.fetch(chrom, elt5p, elt5p+1):
-                info = line.strip()
-                if not tabix:
-                    info = '\t'.join((line.strip(), str(read.positions[0]), str(read.positions[-1])))
+                lib = 'sampled'
 
                 if not tabix:
                     lib = getRG(read.tags)
-                if lib not in overlaps:
-                    overlaps[lib] = dd(list)
 
                 if tabix or not read.is_duplicate:
-                    overlaps[lib]['5pNonDup'].append(info)
+                    overlaps[lib][line.strip()]['5pNonDup'] += 1
 
-                overlaps[lib]['5pAll'].append(info)
+                overlaps[lib][line.strip()]['5pAll'] += 1
 
             # count 3p junction overlaps
             for read in bam.fetch(chrom, elt3p, elt3p+1):
-                info = line.strip()
-                if not tabix:
-                    info = '\t'.join((line.strip(), str(read.positions[0]), str(read.positions[-1])))
+                lib='sampled'
 
                 if not tabix:
                     lib = getRG(read.tags)
-                if lib not in overlaps:
-                    overlaps[lib] = dd(list)
 
                 if tabix or not read.is_duplicate:
-                    overlaps[lib]['3pNonDup'].append(info)
+                    overlaps[lib][line.strip()]['3pNonDup'] += 1
 
-                overlaps[lib]['3pAll'].append(info)
-
-
-    #for lib in overlaps.keys():
-        #print lib
-        #print "*"*30 + " debug " + "*"*30
-        #print '\t'.join(overlaps[lib].keys())
-        #print '\t'.join(map(str, [len(v) for v in overlaps[lib].values()]))
-        #print "*"*67
+                overlaps[lib][line.strip()]['3pAll'] += 1
 
     return overlaps
 
@@ -185,34 +185,53 @@ def main(args):
 
     for lib in real_overlaps:
         normalised[lib] = {}
-        for category in real_overlaps[lib]:
-            normalised[lib][category] = float(len(real_overlaps[lib][category]))/float(counts[lib])
+        for site in real_overlaps[lib]:
+            normalised[lib][site] = {}
+            for category in real_overlaps[lib][site]:
+                normalised[lib][site][category] = float(real_overlaps[lib][site][category])/float(counts[lib])
 
     for lib in sim_overlaps:
         normalised[lib] = {}
-        for category in sim_overlaps[lib]:
-            normalised[lib][category] = float(len(sim_overlaps[lib][category]))/float(counts[lib])
+        for site in sim_overlaps[lib]:
+            normalised[lib][site] = {}
+            for category in sim_overlaps[lib][site]:
+                normalised[lib][site][category] = float(sim_overlaps[lib][site][category])/float(counts[lib])
 
     for lib in normalised:
         enrichment[lib] = {}
-        for category in normalised[lib]:
-            enrichment[lib][category]  = normalised[lib][category] / normalised['sampled'][category]
+        for site in normalised[lib]:
+            enrichment[lib][site] = {}
+            for category in normalised[lib][site]:
+                if normalised['sampled'][site][category] == 0:
+                    enrichment[lib][site][category] = 'inf'
+                else:
+                    enrichment[lib][site][category] = normalised[lib][site][category] / normalised['sampled'][site][category]
 
-    with open(args.outdir + '/' + 'enrichment.txt', 'w') as enrich_out:
-        enrich_out.write("Library\t5p Overlaps\t5p Overlaps (without PCR dups)\t3p Overlaps\t3p Overlaps (without PCR dups)\n")
-        for lib in enrichment:
-            enrich_out.write('\t'.join((lib, 
-                             str(enrichment[lib]['5pAll']),
-                             str(enrichment[lib]['5pNonDup']),
-                             str(enrichment[lib]['3pAll']),
-                             str(enrichment[lib]['3pNonDup'])
-                             )) + '\n')
+    bam = pysam.Samfile(args.bam, 'rb')
+    libs = normalised.keys()
 
-    for lib in real_overlaps:
-        for category in real_overlaps[lib]:
-            with open(args.outdir + '/' + category + '.' + lib + '.txt', 'w') as libout:
-                for rec in real_overlaps[lib][category]:
-                    libout.write(rec + '\n')
+    for lib in real_overlaps.keys():
+        with open(args.outdir + '/' + lib + '.enrichment.txt', 'w') as libout:
+            libout.write("Total reads in library:\t" + str(counts[lib]) + "\n")
+            libout.write("Sampled reads in matched random:\t" + str(counts['sampled']) + "\n")
+            libout.write("Chrom\tStart\tEnd\tStrand\tID\tTE_Start\tTE_End\t5p Overlaps\t5p Matched Random Overlaps\t5p Overlap Enrichment\t5p Overlaps (without PCR dups)\t5p NonDup Matched Random Overlaps\t5p NonDup Overlap Enrichment\t3p Overlaps\t3p Matched Random Overlaps\t3p Overlap Enrichment\t3p Overlaps (without PCR dups)\t3p NonDup Matched Random Overlaps\t3p NonDup Overlap Enrichment\n")
+            with open(args.elts, 'r') as elts:
+                for line in elts:
+                    site = line.strip()
+                    libout.write('\t'.join((site, 
+                                     str(real_overlaps[lib][site]['5pAll']),
+                                     str(sim_overlaps['sampled'][site]['5pAll']),
+                                     str(enrichment[lib][site]['5pAll']),
+                                     str(real_overlaps[lib][site]['5pNonDup']),
+                                     str(sim_overlaps['sampled'][site]['5pNonDup']),
+                                     str(enrichment[lib][site]['5pNonDup']),
+                                     str(real_overlaps[lib][site]['3pAll']),
+                                     str(sim_overlaps['sampled'][site]['3pAll']),
+                                     str(enrichment[lib][site]['3pAll']),
+                                     str(real_overlaps[lib][site]['3pNonDup']),
+                                     str(sim_overlaps['sampled'][site]['3pNonDup']),
+                                     str(enrichment[lib][site]['3pNonDup'])
+                                     )) + '\n')
 
 
 if __name__ == '__main__':

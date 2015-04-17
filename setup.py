391d5a787e0556d41ec4c36509000850ca467c86
Adam Ewing <adam.ewing@gmail.com>
2015-04-18 09:11:09 +1000

    add setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='TEBreak',
+    version='0.0.1',
+    author='Adam Ewing',
+    author_email='adamewing@gmail.com',
+    description=("Insertion finder for high throughput sequence data"),
+    license='MIT',
+    url='https://github.com/adamewing/tebreak',
+    packages=['tebreak'],
+    install_requires = [
+        'pysam>=0.8.1',
+        'bx-python>=0.5.0',
+        'scipy>=0.14.0',
+        'numpy>=1.9.0',
+    ],
+)
+
+

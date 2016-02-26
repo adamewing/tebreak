a311d4858859bcb3dff108650b9521d86f8dc74a
Mischa Lundberg <lundberg.mischa@gmail.com>
2016-02-26 01:25:55 -0500

    changes on unit test
    Former-commit-id: e4dcf117b8a8fdd33911f25405cdfcd9ca4661ed

--- test/unitTest.py
+++ test/unitTest.py
@@ -5,7 +5,8 @@ class MyTestCase(unittest.TestCase):
 
   def test_equal_output(self):
     expected = open("example.tab.reference.txt" , "rw+")
-    output = open("example.tab.txt" , "rw+")
+    sed 's/^.\{36\}//' example.tab.txt > example.tab1.txt
+    output = open("example.tab1.txt" , "rw+")
     self.assertEquals(expected.readlines(), output.readlines())
 
 if __name__ == '__main__':

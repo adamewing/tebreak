133470e791c0e47457c4b7a44f68e5a9a4049fdb
Adam Ewing <adam.ewing@gmail.com>
2016-05-10 13:08:23 +1000

    add script

---
+++ scripts/annotate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import pysam
+
+if len(sys.argv) == 4:
+    tbx = pysam.Tabixfile(sys.argv[2])
+    
+    with open(sys.argv[1]) as l1seq:
+        for line in l1seq:
+
+            if line.startswith('UUID'):
+                header = line.strip().split()
+                header.append(sys.argv[3])
+                print '\t'.join(header)
+                continue
+
+            c = line.strip().split()
+            chrom = c[0]
+            start = int(c[1])
+            end   = int(c[2])
+
+            annotations = []
+
+            if chrom in tbx.contigs:
+                for rec in tbx.fetch(chrom, start, end):
+                    annotations.append('|'.join(rec.strip().split()))
+
+            annotations = list(set(annotations)) # uniqify
+
+            if len(annotations) == 0: annotations.append('NA')
+
+            print line.strip() + '\t' + ','.join(annotations)
+
+else:
+    sys.exit("usage: %s <l1seq.py output> <tabix> <header name>" % sys.argv[0])

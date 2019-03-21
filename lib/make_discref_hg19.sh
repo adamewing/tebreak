224b23adfd5d32193950c6302dace0ba979e0775
Adam Ewing <adam.ewing@gmail.com>
2019-03-21 13:16:42 +1000

    add L1PA to human discordant targets

--- lib/make_discref_hg19.sh
+++ lib/make_discref_hg19.sh
@@ -15,7 +15,10 @@ wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/chromOut.tar.gz -O /
 tar xvzf /tmp/rmsk/chromOut.tar.gz -C /tmp/rmsk
 
 cat /tmp/rmsk/*/*.out | grep 'L1HS' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' > hg19.te.disctgt.txt
+cat /tmp/rmsk/*/*.out | grep 'L1PA2' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' >> hg19.te.disctgt.txt
+cat /tmp/rmsk/*/*.out | grep 'L1PA3' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' >> hg19.te.disctgt.txt
 cat /tmp/rmsk/*/*.out | grep 'AluY' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' >> hg19.te.disctgt.txt
+cat /tmp/rmsk/*/*.out | grep 'AluS' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' >> hg19.te.disctgt.txt
 cat /tmp/rmsk/*/*.out | grep 'SVA_' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' -e 's/Other/SVA/' >> hg19.te.disctgt.txt
 
 

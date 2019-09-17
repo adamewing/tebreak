f41287f810f7ec99304597f4b3b5a23fa8850ce6
adamewing <adamewing@users.noreply.github.com>
2019-09-17 22:21:20 +1000

    Update make_discref_hg38.sh

--- lib/make_discref_hg38.sh
+++ lib/make_discref_hg38.sh
@@ -14,8 +14,8 @@ fi
 wget http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.out.gz -O /tmp/rmsk/hg38.fa.out.gz
 
 zcat /tmp/rmsk/hg38.fa.out.gz | grep 'L1HS' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' > hg38.te.disctgt.txt
-zcat /tmp/rmsk/hg38.fa.out.gz | grep 'L1PA2' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' > hg38.te.disctgt.txt
-zcat /tmp/rmsk/hg38.fa.out.gz | grep 'L1PA3' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' > hg38.te.disctgt.txt
+zcat /tmp/rmsk/hg38.fa.out.gz | grep 'L1PA2' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' >> hg38.te.disctgt.txt
+zcat /tmp/rmsk/hg38.fa.out.gz | grep 'L1PA3' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' >> hg38.te.disctgt.txt
 zcat /tmp/rmsk/hg38.fa.out.gz | grep 'AluY' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' >> hg38.te.disctgt.txt
 zcat /tmp/rmsk/hg38.fa.out.gz | grep 'AluS' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' >> hg38.te.disctgt.txt
 zcat /tmp/rmsk/hg38.fa.out.gz | grep 'SVA_' | awk '{print $5"\t"$6"\t"$7"\t"$11"\t"$10"\t"$9}' | sed -e 's/^chr//' -e 's/C$/-/' -e 's/Other/SVA/' >> hg38.te.disctgt.txt
